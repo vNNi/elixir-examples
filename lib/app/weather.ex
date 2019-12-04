374265f6a8cae0186dcd07220ac62cb13357e6f9
Vinicius <vinicius_k2013@hotmail.com>
2019-12-03 21:41:55 -0300

    feat: add kelvin to celsius function

--- lib/app/weather.ex
+++ lib/app/weather.ex
@@ -8,4 +8,7 @@ defmodule App.Weather do
     "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{get_appid()}"
   end
 
+  def kelvin_to_celsius(temperature) do
+    (temperature - 273.15 ) |> Float.round(1)
+  end
 end
